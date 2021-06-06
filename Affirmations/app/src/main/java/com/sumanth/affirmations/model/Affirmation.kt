b8e28430f4d64d7e5af6bbb69351875721f3f83f
5um4n7h <sumanthhegde13@gmail.com>
2021-06-06 15:36:33 +0530

    initial commit

---
+++ Affirmations/app/src/main/java/com/sumanth/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.sumanth.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
